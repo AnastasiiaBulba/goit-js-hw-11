64e09c86a1300a6cfbd89452620ea3f6e0ebd60e
Anastasiia Bulba <fedinanastya@gmail.com>
2024-09-13 18:11:05 +0300

    start-2

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { functionName1, functionName2 } from './js/pixabay-api.js';
+import { renderFunction1, renderFunction2 } from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
